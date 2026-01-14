c5a25e0ab85c963bd2889a5e11e520dcba88f12b
shiv-coder <shivi359@gmail.com>
2026-01-14 09:17:46 -0500

    few filws moved

---
+++ Module2/Main.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+ 
+ 
+/*
+    Week 2: Module 2 Learning Activity - Practice Shell
+    Concepts to Practice:
+    - Variables & Constants
+    - if / if…else / nested if
+    - switch statements
+    - while and for loops
+    - Increment (++) / Decrement (--) / Compound Assignment (+=, -=)
+    - Typecasting
+*/
+ 
+ 
+class Student {
+    // STEP 1: Declare instance variables
+    
+ 
+ 
+    // STEP 2: Declare a constant (final)
+ 
+ 
+    // STEP 3: Method to display student info
+    void displayInfo() {
+        // TODO: Print all fields
+    }
+ 
+ 
+    // STEP 4: Conditional statements (if…else)
+    void checkGPA() {
+        // TODO: Use if…else to check GPA ranges
+    }
+ 
+ 
+    // STEP 5: Nested if example
+    void checkProgram() {
+        // TODO: Nested if for program and GPA
+    }
+ 
+ 
+    // STEP 6: Switch statement example
+    void gradeDescription() {
+        // TODO: Use switch to print school type based on gradeLevel
+    }
+ 
+ 
+    // STEP 7: While loop example
+    void printNumbersUpToGPA() {
+        // TODO: Use while loop, typecast GPA to int
+    }
+ 
+ 
+    // STEP 8: For loop example
+    void printMultiplesOfID() {
+        // TODO: Use for loop to print multiples of ID
+    }
+ 
+ 
+    // STEP 9: Increment / Decrement / Compound assignment example
+    void updateGPA() {
+        // TODO: Demonstrate ++, --, +=, -= with GPA
+    }
+}
+ 
+ 
+ 
+// MAIN CLASS
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+ 
+ 
+        System.out.println("==== Week 2: Module 2 Learning Activity ====");
+ 
+ 
+        // STEP 1: Create a Student object
+        // Student s1 = new Student();
+ 
+ 
+        // STEP 2: Assign values to fields (or use Scanner input)
+ 
+ 
+        // STEP 3: Call methods to practice all concepts
+        
+    }
+}
+ 
+ 
