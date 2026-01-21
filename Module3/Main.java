b71144a794be7574b13d8a62bf04b701e9d5768f
shiv-coder <shivi359@gmail.com>
2026-01-21 10:58:28 -0500

    program completed

--- Module3/Main.java
+++ Module3/Main.java
@@ -90,33 +90,55 @@ public class Main {
       
         // STEP 1: Call a method to greet the user
         // Example: greetUser();
+        greetUser();
  
  
         // STEP 2: Call a method that adds two numbers and returns the sum
         // Example: addNumbers(?, ?);
+
+        int sum = addNumbers(77, 19);
+        System.out.println("Sum of 77 and 19= " + sum);
  
  
         // STEP 3: Call a method that multiplies two numbers and returns the product
         // Example: multiplyNumbers(?, ?);
+
+        int product = multiplyNumbers(19, 17);
+        System.out.println("Product of 19 and 17 is: " + product);
+
  
  
         // STEP 4: Call a method that checks if a number is positive and even using logical operators
         // Example: isPositiveAndEven(?);
+        
+        boolean check = isPositiveAndEven(12);
+        System.out.println(" is positive and even " + check);
  
  
         // STEP 5: Call overloaded methods (same name, different parameters)
         // Example: displayInfo();
         // Example: displayInfo("John");
         // Example: displayInfo("John", 25);
+
+        displayInfo();
+        displayInfo("Shivi");
+        displayInfo("Alice", 20);
  
  
         // STEP 6: Enum usage demo
         // Example: Choice userChoice = Choice.ROCK;
         // Example: Choice computerChoice = Choice.SCISSORS;
+
+        Choice userChoice = Choice.ROCK;
+        Choice computerChoice = Choice.SCISSORS;
+        showChoice("Player", userChoice);
+        showChoice("Computer", computerChoice);
  
  
         // STEP 7: Method call stack demo by calling other methods
         // Example: sumAndMultiply(?, ?);
+        int result = sumAndMultiply(3,5);
+        System.out.println("Sumand multiply result = " + result);
  
  
         scanner.close();
