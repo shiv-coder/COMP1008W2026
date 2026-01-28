a0c70513aad36af21a1eeef5909afeb88b631284
shiv-coder <shivi359@gmail.com>
2026-01-28 09:47:59 -0500

    Added arrayList Demo

---
+++ Module4/ArrayListDemo.java
@@ -0,0 +1,25 @@
+ // LESSON 4 - PART 2
+import java.util.ArrayList;
+ 
+public class ArrayListDemo {
+    public static void main(String[] args) {
+ 
+        // STEP 1: Create an ArrayList of Strings
+        
+ 
+        // STEP 2: Add elements to the ArrayList
+        
+ 
+        // STEP 3: Print all elements
+       
+ 
+        // STEP 4: Remove an element
+       
+ 
+        // STEP 5: Access element by index
+       
+ 
+        // STEP 6: Check if a fruit exists
+        
+} 
+}
