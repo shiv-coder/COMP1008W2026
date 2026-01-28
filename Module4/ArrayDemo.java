4e749b4a2cb4b8c795944cb25c96f8ec4e419c12
shiv-coder <shivi359@gmail.com>
2026-01-28 10:08:02 -0500

    complete ArrayDemo file

--- Module4/ArrayDemo.java
+++ Module4/ArrayDemo.java
@@ -3,15 +3,34 @@ public class ArrayDemo {
     public static void main(String[] args) {
  
         // STEP 1: Declare and initialize an integer array
-       
- 
+       int[] numbers = {10,30,50,70,90};
+        
         // STEP 2: Access and print each element
        
+       System.out.println("Accesing array elements: ");
+       for(int i=0;i<numbers.length;i++){
+
+         System.out.println("Index" + i +":" + numbers[i]);
+       }
  
         // STEP 3: Modify an element
+        numbers[1] = 35;
+        //Enhanced for loop
+        //for(int variable: Array/Collectin){
+    //code    
+    //}
+    for( int num : numbers){
+        System.out.println(num);
+    }
+
        
  
         // STEP 4: Find the sum of all elements
+        int sum = 0;
+        for(int num: numbers){
+            sum += num;
+        }
+        System.out.println("\nSum of the elements: " + sum);
         
         
     }
