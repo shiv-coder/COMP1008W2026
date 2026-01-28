4438f158613e08d11a76a96e9c8a8c6c744195e3
shiv-coder <shivi359@gmail.com>
2026-01-28 09:45:55 -0500

    ArrayDemo

---
+++ Module4/ArrayDemo.java
@@ -0,0 +1,19 @@
+// LESSON 4 - PART 1
+public class ArrayDemo {
+    public static void main(String[] args) {
+ 
+        // STEP 1: Declare and initialize an integer array
+       
+ 
+        // STEP 2: Access and print each element
+       
+ 
+        // STEP 3: Modify an element
+       
+ 
+        // STEP 4: Find the sum of all elements
+        
+        
+    }
+}
+ 
