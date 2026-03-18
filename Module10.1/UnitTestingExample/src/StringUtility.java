581849ee252d0fdfdcc201f71ff82dbdcc4b284b
shiv-coder <shivi359@gmail.com>
2026-03-18 09:53:09 -0400

    completed StringUtility class

--- Module10.1/UnitTestingExample/src/StringUtility.java
+++ Module10.1/UnitTestingExample/src/StringUtility.java
@@ -2,14 +2,35 @@ public class StringUtility {
  
  
     public String reverse(String str){
+
+        if(str == null){
+            throw new IllegalArgumentException("String cannot be null");
+        }
+
+        String reversed ="";
+
+        for(int i = str.length()-1; i>=0;i--){
+            reversed += str.charAt((i));
+        }
  
- 
+        return reversed;
         
     }
+
+    //String str = "Hello"
+    //Str.length()=5
+    //Last validIndex = 4
  
  
     public boolean isPalindrome(String str){
- 
+
+             if(str == null){
+            throw new IllegalArgumentException("String cannot be null");
+        }
+
+        String reversed = reverse(str);
+
+        return str.equalsIgnoreCase(reversed);
  
     }
 }
