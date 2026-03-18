6e629642082e1bc2278d87e9218c58c5f676dcbe
shiv-coder <shivi359@gmail.com>
2026-03-18 10:36:04 -0400

    upadted

--- Module10.1/UnitTestingExample/src/BigTestSuite.java
+++ Module10.1/UnitTestingExample/src/BigTestSuite.java
@@ -12,32 +12,39 @@ public class BigTestSuite {
  
     @Test
     public void testAdd() {
+        assertEquals(10,calc.add(3,4));
         
     }
  
  
     @Test
     public void testSubtract() {
+        assertEquals(2,calc.subtract(6, 4));
         
     }
  
  
     @Test
     public void testMultiply() {
-        
+        assertEquals(10,calc.multiply(5,2));
     }
  
  
     @Test
     public void testDivide() {
-        
+        assertEquals(1,calc.divide(4,2));
     }
  
  
     // Exception test (division by zero)
-   
- 
- 
+   @Test(expected = ArithmeticException.class)
+
+   public void testDivideByZero(){
+
+    calc.divide(10, 0);
+
+   }
+  
  
     // StringUtility tests
     StringUtility strUtil = new StringUtility();
@@ -45,19 +52,22 @@ public class BigTestSuite {
  
     @Test
     public void testReverse() {
+        assertEquals("OLLEH",strUtil.reverse("HELLO"));
         
     }
  
  
     @Test
     public void testPalindromeTrue() {
+        assertTrue(strUtil.isPalindrome("madam"));
         
     }
  
  
     @Test
     public void testPalindromeFalse() {
-        
+
+          assertFalse(strUtil.isPalindrome("hello"));
     }
  
  
@@ -70,9 +80,9 @@ public class BigTestSuite {
  
         BankAccount acc = new BankAccount("John",500);
         acc.deposit(200);
- 
- 
-       
+        assertEquals(700,acc.getBalance(),0.001);
+        
+          
     }
  
  
@@ -82,8 +92,7 @@ public class BigTestSuite {
  
         BankAccount acc = new BankAccount("John",500);
         acc.withdraw(200);
- 
- 
+         assertEquals(300,acc.getBalance(),0.001); 
        
     }
  
@@ -91,16 +100,16 @@ public class BigTestSuite {
  
  
     // Exception test: withdraw more than balance
-    
- 
+    @Test(expected = IllegalArgumentException.class)
+    public void testWithdrawTooMuch() {
+        BankAccount acc = new BankAccount("John", 500);
+        acc.withdraw(600);
     }
- 
- 
- 
- 
+
     // Exception test: negative deposit
-   
- 
- 
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeDeposit() {
+        BankAccount acc = new BankAccount("John", 500);
+        acc.deposit(-100);
     }
 }
